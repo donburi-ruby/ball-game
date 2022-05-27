0a88df1c501c56856cd589083f9fa6d7d936080a
SSumimoto <s205026@matsu.shimane-u.ac.jp>
2022-05-27 16:20:36 +0900

    main.rbを編集，画像ファイルを追加

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 require 'dxruby'
+require_relative 'Player'
 #テスト
+x = 200
+y = 150
+enemy_image = Image.load('./yellowBall.png')
+enemy = Player.new(x, y, enemy_image)
+
+Window.loop do
+    Sprite.draw(enemy)
+end
