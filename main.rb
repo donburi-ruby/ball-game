8aae13c9a1661a1f6abb3596f7f65003de5b3d03
SSumimoto <s205026@matsu.shimane-u.ac.jp>
2022-05-29 00:56:18 +0900

    ballとbarを表示

--- main.rb
+++ main.rb
@@ -1,11 +1,29 @@
 require 'dxruby'
-require_relative 'Player'
+require_relative 'Ball'
+require_relative 'Bar'
+
 #テスト
-x = 200
-y = 150
-enemy_image = Image.load('./yellowBall.png')
-enemy = Player.new(x, y, enemy_image)
+# Window Size 
+windowH, windowW = 480, 640
+
+# ball settings
+ball_image = Image.load('./yellowBall.png')
+ballX, ballY = windowW/2-ball_image.width/2, windowH/2-ball_image.height/2
+ball = Ball.new(ballX, ballY, ball_image)
+
+# bar settings
+bar_image = Image.new(10, 200, [255, 255, 255, 255])
+
+# left bar settings
+barlX, barlY = 50, windowH/2-bar_image.height/2
+bar_left = Bar.new(barlX, barlY, bar_image)
+
+# right bar settings
+barrX, barrY = windowW-bar_image.width-50, windowH/2-bar_image.height/2
+bar_right = Bar.new(barrX, barrY, bar_image)
 
 Window.loop do
-    Sprite.draw(enemy)
+    ball.draw()
+    bar_left.draw()
+    bar_right.draw()
 end
