046ec3586586f17ca9ad4af1855187c96bbde0a2
SSumimoto <s205026@matsu.shimane-u.ac.jp>
2022-06-02 00:22:02 +0900

    ボールがバーの横に当たったときの跳ね返りを追加

--- main.rb
+++ main.rb
@@ -22,8 +22,33 @@ barLeft = Bar.new(barLX, barLY, barImage)
 barRX, barRY = windowW-barImage.width-50, windowH/2-barImage.height/2
 barRight = Bar.new(barRX, barRY, barImage)
 
+# ball speed for debug
+speed = 3
+
 Window.loop do
     ball.draw()
     barLeft.draw()
     barRight.draw()
+
+    # get ball side location
+    ballLeftX = ball.x
+    ballSideY = ball.y + ball.image.height/2
+    ballRightX = ballLeftX+ball.image.width
+
+    # move ball
+    ball.x += speed
+
+    # check hitting of ball & right bar
+    if(ballSideY>=barRight.y && ballSideY<=barRight.y+barRight.image.height && ballRightX>=barRight.x && ballRightX<=barRight.x+speed)
+        speed *= -1
+    end
+
+    # check hitting of ball & left bar
+    if(ballSideY>=barLeft.y && ballSideY<=barLeft.y+barLeft.image.height && ballLeftX<=barLeft.x+barLeft.image.width && ballLeftX>=barLeft.x+barLeft.image.width+speed)
+        speed *= -1
+    end
+
+    # controll bars for debug
+    barLeft.y += Input.y
+    barRight.y += Input.y
 end
