0e18bb9a4a46735cdfac6f1e6b9707d124532766
Farah Afifah <farahafifah00323@gmail.com>
2022-06-03 15:45:02 +0900

    Ball.rbの名前変更

--- ball.rb
+++ ball.rb
@@ -1,2 +1,18 @@
 class ball > Sprite
+    def initialize(image)
+     @ball = image
+     @speedX = rand(3)+1
+     @speedY = rand(3)+1
+     super
+    end
+
+    def draw
+      Sprite.draw(@ball)
+    end
+
+    def move
+        @ball.x
+
+
+
 end
