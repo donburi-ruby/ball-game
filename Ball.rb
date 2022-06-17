73f53264a64cb6936814bbf1b6066e9f7c69ec74
SSumimoto <s205026@matsu.shimane-u.ac.jp>
2022-06-17 16:14:01 +0900

    加速機能とランダムでの跳ね返り機能を追加

--- Ball.rb
+++ Ball.rb
@@ -20,6 +20,11 @@ class Ball < Sprite
 
     def boundX
       @speedX *= -1
+      @speedX += @speedX<0 ? -1 :1
+      @speedY = 0
+      while(@speedY==0)
+        @speedY = rand(0..8)-4
+      end
     end
 
     def boundY
@@ -27,14 +32,14 @@ class Ball < Sprite
     end
 
     def setSpeed
-      @speedX = 0
-      while(@speedX==0)
-        @speedX = rand(0..6)-3
-      end
-      @speedY = 0
-      while(@speedY==0)
-        @speedY = rand(0..6)-3
-      end
+      @speedX = 1
+      # while(@speedX==0)
+      #   @speedX = rand(0..6)-3
+      # end
+      @speedY = 1
+      # while(@speedY==0)
+      #   @speedY = rand(0..6)-3
+      # end
     end
 
 end
